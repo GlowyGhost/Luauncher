2a302da10b316b15f8b8fd84a1bff3bc89c0896d
GlowyGhost <jakubklimczak2013@gmail.com>
2025-07-30 13:41:23 +0100

    Update output_screen.dart

--- flutter/lib/screens/output_screen.dart
+++ flutter/lib/screens/output_screen.dart
@@ -10,23 +10,42 @@ class OutputScreen extends StatefulWidget {
 }
 
 class _OutputScreenState extends State<OutputScreen> {
-  	@override
-    Widget build(BuildContext context) {
-		return Scaffold(
-			body: AnimatedBuilder(
-				animation: logger,
-				builder: (context, _) {
-					return ListView.builder(
-						padding: const EdgeInsets.all(16),
-						itemCount: logger.logs.length,
-						itemBuilder: (context, index) {
-							final log = logger.logs[index];
-							return Text(log, style: TextStyle(fontSize: 18, color: settings.oldDarkMode ? Color(0xFFFFFFFF) : Colors.black));
-						},
-					);
-				},
-			),
-		);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10, 10, 10, 5),
+					child: Row(
+						children: [
+							ElevatedButton.icon(
+								onPressed: logger.clear,
+								icon: const Icon(Icons.clear),
+								label: Text("Clear", style: TextStyle(fontSize: 18))
+							)
+						],
+					)
+        ),
+
+        Expanded(
+          child: Scaffold(
+            body: AnimatedBuilder(
+              animation: logger,
+              builder: (context, _) {
+                return ListView.builder(
+                  padding: const EdgeInsets.all(16),
+                  itemCount: logger.logs.length,
+                  itemBuilder: (context, index) {
+                    final log = logger.logs[index];
+                    return Text(log, style: TextStyle(fontSize: 18, color: settings.oldDarkMode ? Color(0xFFFFFFFF) : Colors.black));
+                  },
+                );
+              },
+            ),
+          )
+        )
+      ],
+    );
   }
 }
 
