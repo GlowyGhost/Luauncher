b8a6c92615efb303d669f1b853180b081f61c459
GlowyGhost <jakubklimczak2013@gmail.com>
2025-08-30 11:03:30 +0100

    Update main.rs

--- src-tauri/updater/src/main.rs
+++ src-tauri/updater/src/main.rs
@@ -6,27 +6,49 @@ use std::{
     io::{stdout, Write},
     thread,
     time::Duration,
-    fs,
+    fs::{self, File},
     path::Path,
-    process::Command
+    process::Command,
+    net::TcpStream,
 };
+use reqwest::blocking::Client;
+use serde::Deserialize;
+use zip::ZipArchive;
 
 fn presskeytoquit() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Press any key to exit...");
+    print!("Press any key to exit. This window will close in 10 seconds.");
     stdout().flush()?;
+
+    let mut seconds = 10;
+    let mut elapsed_ms = 0;
+    let tick_ms = 100;
 
     while event::poll(Duration::from_millis(0))? {
         let _ = event::read()?;
     }
     loop {
-        if event::poll(Duration::from_millis(100))? {
+        if event::poll(Duration::from_millis(0))? {
             if let Event::Key(_) = event::read()? {
                 break;
             }
         }
+
+        if elapsed_ms % 1000 == 0 {
+            print!("\rPress any key to exit. This window will close in {} seconds.    ", seconds);
+            stdout().flush()?;
+            seconds -= 1;
+
+            if seconds < 0 {
+                println!();
+                println!();
+                break;
+            }
+        }
+
+        thread::sleep(Duration::from_millis(tick_ms));
+        elapsed_ms += tick_ms;
     }
 
-    println!();
     Ok(())
 }
 
@@ -133,9 +155,271 @@ fn is_folder_unlocked(path: String) -> bool {
         Err(_) => false,
     }
 }
+fn install_update(old_path: String) -> Result<(), Box<dyn std::error::Error>> {
+    print!("\rPreparing to download...");
+    let client = Client::new();
+    let release: Release = client
+        .get("https://api.github.com/repos/GlowyGhost/Luauncher/releases/latest")
+        .header("User-Agent", "Luauncher-Updater")
+        .send()?
+        .json()?;
+
+    #[cfg(target_os = "windows")]
+    let target_asset = release.assets.iter().find(|a| a.name.ends_with("windows.zip"));
+
+    #[cfg(target_os = "linux")]
+    let target_asset = release.assets.iter().find(|a| a.name.ends_with("linux.zip"));
+
+    #[cfg(target_os = "macos")]
+    let target_asset = release.assets.iter().find(|a| a.name.ends_with("macos.zip"));
+
+    let target_asset = match target_asset {
+        Some(asset) => asset,
+        None => {
+            print!("\rNo suitable release asset found.");
+            return Err("None".into());
+        }
+    };
+
+    print!("\rDownloading {}", target_asset.name);
+    let resp = client.get(&target_asset.browser_download_url).send()?.bytes()?;
+
+    let tmp_zip = Path::new("update_tmp.zip");
+    fs::write(&tmp_zip, &resp)?;
+
+    print!("\rExtracting...");
+
+    let file = File::open(&tmp_zip)?;
+    let mut archive = ZipArchive::new(file)?;
+
+    let extract_dir = Path::new("update_tmp");
+    if extract_dir.exists() {
+        fs::remove_dir_all(extract_dir)?;
+    }
+    fs::create_dir_all(extract_dir)?;
+
+    #[cfg(target_os = "windows")]
+    let expected_name = "Luauncher.exe";
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
+    let expected_name = "Luauncher";
+
+    let mut found = false;
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        let name = Path::new(file.name())
+            .file_name()
+            .and_then(|n| n.to_str())
+            .unwrap_or("");
+
+        if name == expected_name {
+            let mut outfile = File::create(&extract_dir.join(name))?;
+            std::io::copy(&mut file, &mut outfile)?;
+            found = true;
+            break;
+        }
+    }
+
+    if !found {
+        print!("\rBinary '{}' not found in archive.", expected_name);
+        fs::remove_file(tmp_zip)?;
+        fs::remove_dir_all(extract_dir)?;
+        return Err("None".into());
+    }
+
+    let new_binary = extract_dir.join(expected_name);
+    fs::copy(&new_binary, &old_path)?;
+    print!("\rUpdated successfully to {}!", old_path);
+
+    fs::remove_file(tmp_zip)?;
+    fs::remove_dir_all(extract_dir)?;
+
+    Ok(())
+}
+
+#[derive(Deserialize, Debug)]
+struct Release {
+    assets: Vec<Asset>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Asset {
+    name: String,
+    browser_download_url: String,
+}
 
 fn update(old_path: String) -> Result<(), Box<dyn std::error::Error>> {
-    println!("{}", old_path);
+    print!("Verifying Internet Connection...");
+    stdout().flush()?;
+
+    let connected = TcpStream::connect_timeout(
+        &("1.1.1.1:80".parse().unwrap()),
+        Duration::from_secs(2)
+    ).is_ok();
+
+    if !connected {
+        print!("\rNo Internet Connection.            ");
+        println!();
+        println!();
+
+        println!("Update Failed. Internet connection is required to update.");
+        println!("Please check your internet connection and try again.");
+        return presskeytoquit();
+    }
+
+    print!("\rInternet Connection Verified.");
+    println!();
+    println!();
+
+    println!("Please do not unplug your device or disconnect from the internet during the update process.");
+    println!();
+    println!();
+
+    stdout().flush()?;
+
+    //Close Luauncher if running
+    stdout().flush()?;
+    print!("Closing Luauncher...");
+
+    let _ = close_luauncher(old_path.clone());
+
+    //Wait for Unlock
+    print!("\rWaiting for Unlock...");
+    stdout().flush()?;
+
+    #[cfg(target_os = "windows")]
+    let path_com = env!("LOCALAPPDATA").to_string() + "\\com.glowyghost.luauncher";
+
+    #[cfg(target_os = "linux")]
+    let path_com = std::env::var("HOME").unwrap_or_default() + "/.local/share/com.glowyghost.luauncher";
+
+    #[cfg(target_os = "macos")]
+    let path_com = std::env::var("HOME").unwrap_or_default() + "/Library/Application Support/com.glowyghost.luauncher";
+
+    loop {
+        if is_folder_unlocked(path_com.clone()) {
+            break;
+        }
+        if !Path::new(&path_com).exists() {
+            break;
+        }
+
+        thread::sleep(Duration::from_millis(1000));
+    }
+
+    print!("\rClosed Luauncher...         ");
+    stdout().flush()?;
+    println!();
+    println!();
+
+    let mut errored = false;
+
+    //Download Update
+    match install_update(old_path.clone()) {
+        Ok(_) => {
+            print!("\rDownloaded Update...        ");
+        }
+        Err(e) => {
+            if e.to_string() != "None" {
+                print!("\rFailed to download update: {}", e);
+            }
+            
+            println!();
+            println!("You'll need to update app file manually.");
+            println!("Other folders will be handled manually unless errored.");
+
+            errored = true;
+        }
+    }
+    println!();
+    stdout().flush()?;
+
+    //com.glowyghost.luauncher deletion
+    stdout().flush()?;
+    println!();
+
+    print!("Deleting WebView data...");
+
+    let res_com = delete_folder(path_com.clone());
+
+    match res_com {
+        Ok(_) => {
+            print!("\rDeleted WebView data...     ");
+        }
+        Err(e) => {
+            if e.to_string().contains("Directory does not exist") {
+                print!("\rDirectory \"{}\" does not exist. It's been deleted already.", path_com);
+            } else {
+                print!("\rFailed to delete folder \"{}\": {}", path_com, e);
+                errored = true;
+            }
+        }
+    }
+    println!();
+    println!();
+    stdout().flush()?;
+
+    print!("Opening new version...");
+    #[cfg(target_os = "windows")]
+    {
+        let escaped = old_path.replace("&", "^&");
+
+        let _ = Command::new("cmd")
+            .args(["/C", "start", "", &escaped])
+            .spawn();
+    }
+
+    #[cfg(target_os = "macos")]
+    {
+        Command::new("open")
+            .arg(&old_path)
+            .spawn();
+    }
+
+    #[cfg(target_os = "linux")]
+    {
+        Command::new(&old_path)
+            .spawn();
+    }
+
+    print!("\rOpened new version...      ");
+    stdout().flush()?;
+    println!();
+    println!();
+
+    println!("MIT License:");
+    println!("Copyright (c) 2025 GlowyGhost
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the \"Software\"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.");
+    stdout().flush()?;
+
+    println!();
+
+    if errored {
+        println!("Update process Failed Somewhere.");
+        println!("You may need to delete some files your self.");
+    } else {
+        println!("Update Successful.");
+        println!("We hope you have a great time continuing to use Luauncher!");
+    }
+
+    println!();
 
     presskeytoquit()
 }
@@ -173,6 +457,10 @@ fn uninstall(old_path: String) -> Result<(), Box<dyn std::error::Error>> {
         thread::sleep(Duration::from_millis(tick_ms));
         elapsed_ms += tick_ms;
     }
+
+    println!("Please do not unplug your device during the uninstall process.");
+    println!();
+    println!();
 
     //Close Luauncher if running
     stdout().flush()?;
@@ -220,7 +508,7 @@ fn uninstall(old_path: String) -> Result<(), Box<dyn std::error::Error>> {
 
     match res_com {
         Ok(_) => {
-            print!("\rDeleted WebView data...");
+            print!("\rDeleted WebView data...     ");
         }
         Err(e) => {
             if e.to_string().contains("Directory does not exist") {
@@ -252,7 +540,7 @@ fn uninstall(old_path: String) -> Result<(), Box<dyn std::error::Error>> {
 
     match res_save {
         Ok(_) => {
-            print!("\rDeleted Save Data...");
+            print!("\rDeleted Save Data...     ");
         }
         Err(e) => {
             if e.to_string().contains("Directory does not exist") {
@@ -269,15 +557,13 @@ fn uninstall(old_path: String) -> Result<(), Box<dyn std::error::Error>> {
     stdout().flush()?;
     println!();
 
-    println!("{}", old_path);
-
     print!("Deleting Application...");
 
     let res_app = delete_file(old_path.clone());
 
     match res_app {
         Ok(_) => {
-            print!("\rDeleted Application...");
+            print!("\rDeleted Application...    ");
         }
         Err(e) => {
             if e.to_string().contains("Directory does not exist") {
