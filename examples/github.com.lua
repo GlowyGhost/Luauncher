0691f36cd9dec9e47e71a4228f08a285297d84ef
GlowyGhost <jakubklimczak2013@gmail.com>
2025-08-30 13:50:12 +0100

    Update github.com.lua

--- examples/github.com.lua
+++ examples/github.com.lua
@@ -3,5 +3,3 @@ openURL("https://github.com/") -- Opens Github.
 waitUntilWindowOpen("chrome.exe", 100000) -- Waits until browser opens for 100s
 
 sleep(3000) -- Wait for 3 seconds
-
-closeLauncherWindow(0) -- Closes Luauncher
